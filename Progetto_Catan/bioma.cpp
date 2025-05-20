e4c6d6bcf0999ea4d4a2a56d722f37fff7bb0915
rotaalessandro <alessandro.rota3@studenti.liceomascheroni.it>
2025-05-20 22:24:14 +0200

    estrazione dadi

--- Progetto_Catan/bioma.cpp
+++ Progetto_Catan/bioma.cpp
@@ -1,10 +1,19 @@
 #include <iostream>
 #include <SFML/Graphics.hpp>
+#include <cstdlib>   
+#include <ctime>
 #include "SFML\Window.hpp"
 #include "SFML\System.hpp"
 #include "header.h"
 #include "bioma.h"
+using namespace std;
 
 bioma::bioma(int a, int b, int c) : type(a), numero(b), colore(c) {}
 int bioma::get_type() { return type; }
-int bioma::produzione_risorsa() {}
+int bioma::Tiro_dadi() {
+    srand(time(NULL));
+    int dado1 = (std::rand() % 6) + 1; // valore tra 1 e 6
+    int dado2 = (std::rand() % 6) + 1; // valore tra 1 e 6
+	cout << "Tiro di dadi effettuato: valore1: " << dado1 << " valore2: " << dado2 << endl;
+    return dado1 + dado2;
+}
